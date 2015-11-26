9b4fb33a50ea533f10478f300a7a4cb05061d60c
Jitendra Agarwal <jitendraagarwal@jitendras-mac-mini.local>
2015-11-26 18:02:37 +0530

    Initial Commit

---
+++ ClearNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClearNote
+//
+//  Created by Jitendra Agarwal on 26/11/15.
+//  Copyright © 2015 Jitendra Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClearNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClearNote
+//
+//  Created by Jitendra Agarwal on 26/11/15.
+//  Copyright © 2015 Jitendra Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
